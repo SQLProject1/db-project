652fc92df0dca509a995f34175f1198eb92c4c21
EgliMNG <luca.egli@mng.ch>
2025-11-07 23:23:54 +0100

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,78 @@
+from flask import Flask, redirect, render_template, request, url_for
+from mysql.connector import pooling
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_CONFIG = {
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_DATABASE")
+}
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+pool = pooling.MySQLConnectionPool(pool_name="pool", pool_size=5, **DB_CONFIG)
+
+def get_conn():
+    return pool.get_connection()
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        conn = get_conn()
+        try:
+            cur = conn.cursor(dictionary=True)
+            cur.execute("SELECT id, content, date FROM todos ORDER BY date")
+            todos = cur.fetchall()
+        finally:
+            try:
+                cur.close()
+            except:
+                pass
+            conn.close()
+
+        return render_template("main_page.html", todos=todos)
+
+    # POST
+    content = request.form["contents"]
+    date = request.form["due_at"]
+    conn = get_conn()
+    try:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO todos (content, `date`) VALUES (%s, %s)", (content, date, ))
+        conn.commit()
+    finally:
+        try:
+            cur.close()
+        except:
+            pass
+        conn.close()
+
+    return redirect(url_for("index"))
+
+@app.post("/complete")
+def complete():
+
+    # POST
+    todo_id = request.form.get("id")
+    conn = get_conn()
+    try:
+        cur = conn.cursor()
+        cur.execute("DELETE FROM todos WHERE id=%s", (todo_id,))
+        conn.commit()
+    finally:
+        try:
+            cur.close()
+        except:
+            pass
+        conn.close()
+
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run()
