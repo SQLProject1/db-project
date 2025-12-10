09a390df4084e415205fdadba88ee72a02c6fe25
Ethunder11111111111131 <ethan.wilson@mng.ch>
2025-12-10 11:53:47 +0100

    Update TODOS.sql

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -11,3 +11,19 @@ CREATE TABLE todos (
     due DATETIME,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE TABLE kriminellen ( 
+foto INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(40) ,
+    geburtsdatum CHAR(10),
+    rasse VARCHAR(20),
+    haftstatus BOOL,
+    geschlecht VARCHAR(10)
+);
+
+CREATE TABLE verbrechen (
+    anklagepunkt INT AUTO_INCREMENT PRIMARY KEY,
+    verbrechenstyp VARCHAR(20)
+    );
+    
+    
