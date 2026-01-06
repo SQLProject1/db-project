68c8d8c3281bb6cf89a00d7a0e2c4a99505fd1c6
SQLProject1 <julien.dornier@mng.ch>
2026-01-06 07:53:29 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -169,3 +169,10 @@ def add_todo():
     db_write(sql, params)
     flash("Todo erfolgreich hinzugefügt!", "success")
     return redirect(url_for("index"))
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
