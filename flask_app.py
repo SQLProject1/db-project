8be4aabfd6203218579ae268a6cae53335961676
Ethunder11111111111131 <ethan.wilson@mng.ch>
2026-01-07 11:47:02 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -205,7 +205,18 @@ def add_verbrechen():
         VALUES (%s, %s, %s, %s)
     """, (verbrechenstyp, geldstrafe, gefaengniszeit, vergehen))
 
-    return redirect(url_for("index"))
+@app.route("/gefaengnis", methods=["POST"])
+@login_required
+def add_gefaengnis():
+    ort = request.form["ort"]
+    sicherheitslevel = request.form["sicherheitslevel"]
+
+    db_write(
+        "INSERT INTO gefaengnis (Ort, Sicherheitslevel) VALUES (%s, %s)",
+        (ort, sicherheitslevel)
+    )
+
+   
 
 
 
